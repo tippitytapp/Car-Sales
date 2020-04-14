5243984a8b44edb13fcbf9e5a754bf376f12ea59
tippitytapp <marc.tapp@gmail.com>
2020-04-14 17:28:50 -0400

    added initial stte and reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+//library imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {createStore} from "redux"
+// component imports
 import App from './App';
-
+import {reducer} from './reducers'
+//styling imports
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
